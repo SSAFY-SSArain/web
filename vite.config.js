958440351d57fa52c7549bc497965dcdd1629f15
whitecity01 <kh081104@gmail.com>
2026-08-16 00:18:43 +0900

    config: 서버 주소변경

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://ec2-13-124-222-178.ap-northeast-2.compute.amazonaws.com",
-        changeOrigin: true
-      }
-    }
-  }
+      '/api': {
+        target: 'http://ssarain.kro.kr',
+        changeOrigin: true,
+      },
+    },
+  },
 });
